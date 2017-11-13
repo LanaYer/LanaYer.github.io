829c60cac241156b0c3288f21210c01971af1fa0
LanaYer <yermolaeva.lana@gmail.com>
2017-11-14 01:20:53 +0300

    del

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=portfolio',
-    'username' => 'portfolio',
-    'password' => '123',
-    'charset' => 'utf8',
-];
--- config/params.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-return [
-    'adminEmail' => 'yermolaeva.lana@gmail.com',
-];
