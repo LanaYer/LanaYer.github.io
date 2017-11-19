e6775d9bcf9850345550b1464f6c80e15fc86e89
LanaYer <yermolaeva.lana@gmail.com>
2017-11-19 20:17:56 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -1,2 +1,28 @@
+var need_height = $(window).height()-135;
 
+$(window).scroll(function() {
+	if ($(window).scrollTop() > need_height) {
+	    $("#block-header").css({display: 'block'});
+	}
+	else {
+		$("#block-header").css({display: 'none'});
+	}
+});	
+	
+$( document ).ready(function() {
+ 
+	$('.block-portfolio-show_more-a').on('click',function () {
+        $('.block-portfolio-show_more').show();
+		$('.block-portfolio-show_more-a').hide();
+    });
+	$('.block-portfolio-hide_more-a').on('click',function () {
+		$('.block-portfolio-show_more').hide();
+		$('.block-portfolio-show_more-a').show();
+    });	
+});
+
+
+
+	
+ 
  
