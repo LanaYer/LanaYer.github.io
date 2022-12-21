ea8498e9b3f6411d88b855422446c42419e169dd
Ермолаева Светлана <yermolaeva.lana@gmail.com>
2022-12-22 02:05:37 +0300

    211

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 var need_height = $(window).height()-135;
 
+function followCursor () {
+    const el = document.querySelector('#follow');
+
+    addEventListener('mousemove', (e) => {
+        const target = e.target; // определяем, где находится курсор
+    		if (!target) return;
+
+    	el.style.left = e.pageX - 30 + 'px'; // задаём элементу позиционирование слева
+    	el.style.top = e.pageY - 10 + 'px'; // задаём элементу позиционирование сверху
+	});
+}
+
 $(window).scroll(function() {
 	if ($(window).scrollTop() > need_height) {
 	    $("#block-header").css({display: 'block'});
@@ -10,7 +22,6 @@ $(window).scroll(function() {
 });	
 	
 $( document ).ready(function() {
- 
 	$('.block-portfolio-show_more-a').on('click',function () {
         $('.block-portfolio-show_more').show();
 		$('.block-portfolio-show_more-a').hide();
@@ -18,7 +29,9 @@ $( document ).ready(function() {
 	$('.block-portfolio-hide_more-a').on('click',function () {
 		$('.block-portfolio-show_more').hide();
 		$('.block-portfolio-show_more-a').show();
-    });	
+    });
+
+    followCursor() // вызываем функцию followCursor
 });
 
 
